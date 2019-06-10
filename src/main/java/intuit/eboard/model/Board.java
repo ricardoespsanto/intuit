085ca74acaef2782e28c443c39ae50320a6bd0ac
Ricardo Espirito Santo <ricardo.santo@evrythng.com>
2019-06-10 11:15:39 +0100

    Removing a header

--- src/main/java/intuit/eboard/model/Board.java
+++ src/main/java/intuit/eboard/model/Board.java
@@ -1,8 +1,3 @@
-/*
- * (c) Copyright Reserved EVRYTHNG Limited 2018. All rights reserved.
- * Use of this material is subject to license.
- * Copying and unauthorised use of this material strictly prohibited.
- */
 package intuit.eboard.model;
 
 import javax.persistence.Entity;
